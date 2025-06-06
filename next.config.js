341e1a8f4361ec8da5c5dac0ee644eb531712139
Enzoku <tonyenzoku@gmail.com>
2025-06-06 13:12:47 +0300

    Fix Next.js stability and compatibility errors

--- next.config.js
+++ next.config.js
@@ -4,17 +4,6 @@ const nextConfig = {
   images: {
     domains: ['images.unsplash.com'], // Add any other image domains you'll be using
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': '.',
-    };
-    return config;
-  },
-  // Enable server actions (if needed in the future)
-  // experimental: {
-  //   serverActions: true,
-  // },
 };
 
 module.exports = nextConfig;
